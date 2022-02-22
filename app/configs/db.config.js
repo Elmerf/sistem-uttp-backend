6753f01bcfea7e608768577eff4fa5a19ec54595
Elmer Fiqi <61036187+Elmerf@users.noreply.github.com>
2022-02-23 00:42:08 +0700

    Update db.config.js

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '',
-  DB: 'admindb',
+  HOST: 'sql6.freemysqlhosting.net',
+  USER: 'sql6474693',
+  PASSWORD: 'qQve1RqC92',
+  DB: 'sql6474693',
   dialect: 'mysql',
   pool: {
     max: 5,
